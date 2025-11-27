02f2dfe8aefc9b9e99187dee23bf48d56fadf0d2
AxeLotlol <mokshnot5@gmail.com>
2025-11-27 11:48:41 -0600

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOp/testflywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleOp/testflywheel.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.opModes.TeleOp;
 
 import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;
 
+
+
 import android.os.Build;
 
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -19,6 +21,9 @@ import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.impl.CRServoEx;
+import dev.nextftc.hardware.powerable.SetPower;
+
 import java.time.*;
 
 
@@ -27,11 +32,29 @@ public class testflywheel extends NextFTCOpMode {
 
     public testflywheel() {
         addComponents(
-                new SubsystemComponent(TempHood.INSTANCE/*, Intake.INSTANCE, Spindexer.INSTANCE*/),
+                new SubsystemComponent(TempHood.INSTANCE /*Intake.INSTANCE, Spindexer.INSTANCE*/),
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE
 
         );
+    }
+
+
+
+    public boolean lift=false;
+
+    public boolean running=false;
+
+    public void hood(){
+        if(lift==false){
+            lift=true;
+        }
+        else
+        {
+            if(lift==true){
+                lift=false;
+            }
+        }
     }
 
 
@@ -43,9 +66,29 @@ public class testflywheel extends NextFTCOpMode {
     @Override
     public void onStartButtonPressed() {
         shooter(1500);
+        Gamepads.gamepad1().triangle().whenBecomesTrue(() -> hood());
+        //Gamepads.gamepad1().circle().whenBecomesTrue(() -> TempHood.INSTANCE.HoodPowerZero.schedule());
     }
     @Override
     public void onUpdate(){
+
+        if (lift == true) {
+            if (running == false) {
+                running = true;
+                TempHood.INSTANCE.HoodUp.schedule();
+                //ActiveOpMode.telemetry().addLine("power set up");
+                running = false;
+
+            }
+        }
+        if (lift == false) {
+            if (running == false) {
+                running = true;
+                TempHood.INSTANCE.HoodDown.schedule();
+                //ActiveOpMode.telemetry().addLine("power set down");
+                running = false;
+            }
+        }
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TempHood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TempHood.java
@@ -1,99 +1,75 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
-import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;
-
 import com.bylazar.configurables.annotations.Configurable;
-import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.Pose;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 
-import dev.nextftc.bindings.BindingManager;
-import dev.nextftc.control.ControlSystem;
-import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.ftc.Gamepads;
-import dev.nextftc.hardware.driving.FieldCentric;
-import dev.nextftc.hardware.driving.MecanumDriverControlled;
-import dev.nextftc.hardware.impl.Direction;
-import dev.nextftc.hardware.impl.IMUEx;
-import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.impl.ServoEx;
-
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
-import java.util.function.Supplier;
+import dev.nextftc.hardware.impl.CRServoEx;
+import dev.nextftc.hardware.powerable.SetPower;
+import java.time.*;
 
 
 @Configurable
 public class TempHood implements Subsystem {
 
+
+
     public static final TempHood INSTANCE = new TempHood();
     private TempHood() { }
 
-    private CRServo hoodServo1;
-    private CRServo hoodServo2;
+    private CRServo hoodServo1n;
+    private CRServo hoodServo2n;
 
-    public boolean lift=false;
-
-    public boolean running=false;
-
-    public void hood(){
-        if(lift==false){
-            lift=true;
-        }
-        else if(lift==true){
-            lift=false;
-        }
-
-    }
+    private CRServoEx hoodServo1 = new CRServoEx(() -> hoodServo1n);
+    private CRServoEx hoodServo2 = new CRServoEx(() -> hoodServo2n);
 
 
 
 
-    @Override
-    public Command getDefaultCommand() {
-
-        if(lift==false){
-            if(running==false){
-            hoodServo1.setPower(1);
-            hoodServo2.setPower(-1);
-            //new Delay(0.3);
-            hoodServo1.setPower(0.0);
-            hoodServo2.setPower(0.0);
-            running=true;
-            }
-        }
-        if(lift==true){
-            if(running==false) {
-                hoodServo1.setPower(-1);
-                hoodServo2.setPower(1);
-                //new Delay(0.3);
-                hoodServo1.setPower(0.0);
-                hoodServo2.setPower(0.0);
-                running = true;
-            }
-        }
-        ActiveOpMode.telemetry().addLine(String.valueOf(lift));
-        ActiveOpMode.telemetry().update();
 
 
-        return null;
-    }
+    ParallelGroup HoodRunUp=new ParallelGroup(
+            new SetPower(hoodServo1,1),
+            new SetPower(hoodServo2,-1)
+    );
+
+    ParallelGroup HoodPowerZero=new ParallelGroup(
+            new SetPower(hoodServo1,0),
+            new SetPower(hoodServo2,0)
+    );
+
+    public SequentialGroup HoodUp=new SequentialGroup(
+            HoodRunUp,
+            new Delay(2.0),
+            HoodPowerZero
+    );
+
+    ParallelGroup HoodRunDown=new ParallelGroup(
+            new SetPower(hoodServo1,-1),
+            new SetPower(hoodServo2,1)
+    );
+
+    public SequentialGroup HoodDown=new SequentialGroup(
+            HoodRunDown,
+            new Delay(2.0),
+            HoodPowerZero
+    );
+
+
+
 
     @Override
     public void initialize() {
 
-        hoodServo1= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
-        hoodServo2= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
-        Gamepads.gamepad1().triangle().whenBecomesTrue(()-> hood());
+        hoodServo1n= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
+        hoodServo2n=  ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
+
 
     }
 

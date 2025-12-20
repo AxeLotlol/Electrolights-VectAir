b1a71b03ce9207d57bc84b1a7cae1b594c3f1cef
AxeLotlol <mokshnot5@gmail.com>
2025-12-20 15:33:55 -0600

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/redTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/redTest.java
@@ -3,7 +3,6 @@
 package org.firstinspires.ftc.teamcode.opModes.Auto;
 
 
-import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
 import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;
 
 import com.pedropathing.follower.Follower;
@@ -13,51 +12,33 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 
-import dev.nextftc.bindings.BindingManager;
-import dev.nextftc.control.ControlSystem;
-import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.delays.Delay;
-import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.groups.SequentialGroup;
-import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
-import dev.nextftc.core.units.Angle;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.extensions.pedro.FollowPath;
-import dev.nextftc.extensions.pedro.TurnTo;
-import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
-import dev.nextftc.hardware.impl.CRServoEx;
 import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.IMUEx;
-import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.impl.ServoEx;
-import dev.nextftc.hardware.powerable.SetPower;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-import org.firstinspires.ftc.teamcode.subsystems.DistanceBlue;
 import org.firstinspires.ftc.teamcode.subsystems.DistanceRed;
 
 
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
-import org.firstinspires.ftc.teamcode.subsystems.MotifScanning;
-import org.jetbrains.annotations.Async;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.bylazar.configurables.annotations.Configurable;
-import com.qualcomm.robotcore.hardware.CRServo;
 
 
-@Autonomous(name = "Red Auto Default Position", group = "Autonomous")
+@Autonomous(name = "RedTest", group = "Autonomous")
 @Configurable
-public class redDefaultMoksh extends NextFTCOpMode {
-    public redDefaultMoksh(){
+public class redTest extends NextFTCOpMode {
+    public redTest(){
         addComponents(
                 new SubsystemComponent(Flywheel.INSTANCE,DistanceRed.INSTANCE),
                 BulkReadComponent.INSTANCE,
@@ -80,29 +61,6 @@ public class redDefaultMoksh extends NextFTCOpMode {
 
 
 
-    private static final int APRILTAG_PIPELINE = 8;
-
-
-    private MotorEx intakeMotor;
-
-    int tagId = 0;
-
-
-    private MotorEx transfer1;
-
-    private int newtps;
-    private ServoEx transfer2;
-    private ServoEx transfer3;
-    private ServoEx hoodservo1;
-    private ServoEx hoodservo2;
-    private Command spinFlyWheel1500;
-    private Command intakeMotorOn;
-
-    private CRServo hoodServo1n;
-    private CRServo hoodServo2n;
-
-    private CRServoEx hoodServo1 = new CRServoEx(() -> hoodServo1n);
-    private CRServoEx hoodServo2 = new CRServoEx(() -> hoodServo2n);
 
     public void onInit() {
         telemetry.addLine("Initializing Follower...");
@@ -113,30 +71,14 @@ public class redDefaultMoksh extends NextFTCOpMode {
 
         telemetry.update();
         follower = PedroComponent.follower();
-        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(APRILTAG_PIPELINE);
-        limelight.start();
 
 
         IMUEx imu = new IMUEx("imu", Direction.LEFT, Direction.BACKWARD).zeroed();
 
 
         paths = new Paths(follower);
-        intakeMotor = new MotorEx("intake");
-        transfer1 = new MotorEx("transfer");
-        transfer2 = new ServoEx("transferServo1");
-
-        transfer3 = new ServoEx("transferServo2");
-        hoodservo1 = new ServoEx("hoodServo1");
-        hoodservo2 = new ServoEx("hoodServo2");
 
 
-        spinFlyWheel1500 = new LambdaCommand()
-                .setStart(() -> shooter(1130));
-        intakeMotorOn = new LambdaCommand()
-                .setStart(() -> intakeMotor.setPower(-1));
-        hoodServo1n= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
-        hoodServo2n=  ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
         pathTimer = new Timer();
         actionTimer = new Timer();
         opmodeTimer = new Timer();
@@ -151,112 +93,15 @@ public class redDefaultMoksh extends NextFTCOpMode {
 
 
 
-
-    Command stopFlywheel = new LambdaCommand()
-            .setStart(() -> shooter(0));
-
-    Command intakeMotorOff = new LambdaCommand()
-            .setStart(() -> intakeMotor.setPower(0));
-    Command hoodUp = new LambdaCommand()
-            .setStart(() -> hoodservo1.setPosition(0.2));
-    Command hoodup2 = new LambdaCommand()
-            .setStart(() -> hoodservo2.setPosition(0.2));
-    Command getMotif = new LambdaCommand()
-            .setStart(() -> tagId = MotifScanning.INSTANCE.findMotif());
-
-    Command distance = new LambdaCommand()
-            .setStart(()-> findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
-
-    Command openTransfer = new LambdaCommand()
-            .setStart(()-> {
-                //`5transfer2.setPosition(-0.25);
-                transfer2.setPosition(0.3);
-                //transfer3.setPosition(0.75);
-            });
-    Command closeTransfer = new LambdaCommand()
-            .setStart(() -> {
-                //transfer2.setPosition(1);
-                transfer2.setPosition(1.25);
-                //transfer3.setPosition(0);
-            });
-
-    Command transferOn = new LambdaCommand()
-            .setStart(()-> transfer1.setPower(-1));
-    Command transferOff = new LambdaCommand()
-            .setStart(() -> transfer1.setPower(0));
-
-    ParallelGroup HoodRunUp=new ParallelGroup(
-            new SetPower(hoodServo1,1),
-            new SetPower(hoodServo2,-1)
-    );
-    public ParallelGroup HoodPowerZero=new ParallelGroup(
-            new SetPower(hoodServo1,0),
-            new SetPower(hoodServo2,0)
-    );
-
-    public SequentialGroup HoodUp=new SequentialGroup(
-            HoodRunUp,
-            new Delay(0.1),
-            HoodPowerZero
-    );
-
-    ParallelGroup HoodRunDown=new ParallelGroup(
-            new SetPower(hoodServo1,-1),
-            new SetPower(hoodServo2,1)
-    );
-
-    public SequentialGroup HoodDown=new SequentialGroup(
-            HoodRunDown,
-            new Delay(0.17),
-            HoodPowerZero
-    );
-
     public Command Auto(){
         return new SequentialGroup(
-                spinFlyWheel1500,
                 new FollowPath(paths.preloadLaunch,true,0.8),
-                openTransfer,
-                transferOn,
-                new Delay(2),
-                closeTransfer,
-
-                intakeMotorOn,
                 new FollowPath(paths.intakeRow1,false,0.8),
-                transferOff,
-
                 new FollowPath(paths.launchRow1,true,0.8),
-                openTransfer,
-                transferOn,
-                new Delay(2),
-                closeTransfer,
-
-                intakeMotorOn,
                 new FollowPath(paths.intakeRow2,false,0.7),
-                transferOff,
-
-
                 new FollowPath(paths.launchRow2,true,0.9),
-                intakeMotorOn,
-                openTransfer,
-                transferOn,
-                new Delay(2),
-                closeTransfer,
-
-
-
-                intakeMotorOn,
                 new FollowPath(paths.intakeRow3,false,0.8),
-
-                transferOff,
                 new FollowPath(paths.launchRow3,false,0.8),
-                openTransfer,
-                transferOn,
-                intakeMotorOff,
-
-                new Delay(2),
-                //closeTransfer,
-                transferOff,
-                stopFlywheel,
                 new FollowPath(paths.leavePoints,true,0.9)
 
 
@@ -268,7 +113,6 @@ public class redDefaultMoksh extends NextFTCOpMode {
     public void onStartButtonPressed() {
         opmodeTimer.resetTimer();
         pathTimer.resetTimer();
-        //int tag=MotifScanning.INSTANCE.findMotif();
         Auto().schedule();
         telemetry.update();
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Airsort.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import dev.nextftc.core.commands.conditionals.SwitchCommand;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.hardware.impl.CRServoEx;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.powerable.SetPower;
+
+public class Airsort implements Subsystem {
+    public static final Airsort INSTANCE = new Airsort();
+    private Airsort() { }
+    private CRServo hoodServo1n;
+    private CRServo hoodServo2n;
+
+    private CRServoEx hoodServo1 = new CRServoEx(() -> hoodServo1n);
+    private CRServoEx hoodServo2 = new CRServoEx(() -> hoodServo2n);
+
+
+    public static MotorEx transfer;
+
+
+
+    ParallelGroup HoodRunUp=new ParallelGroup(
+            new SetPower(hoodServo1,1),
+            new SetPower(hoodServo2,-1)
+    );
+
+    public ParallelGroup HoodPowerZero=new ParallelGroup(
+            new SetPower(hoodServo1,0),
+            new SetPower(hoodServo2,0)
+    );
+
+    public SequentialGroup HoodDown=new SequentialGroup(
+            HoodRunUp,
+            new Delay(0.18),
+            HoodPowerZero
+    );
+
+    ParallelGroup HoodRunDown=new ParallelGroup(
+            new SetPower(hoodServo1,-1),
+            new SetPower(hoodServo2,1)
+    );
+
+    public SequentialGroup HoodUp=new SequentialGroup(
+            HoodRunDown,
+            new Delay(0.17),
+            HoodPowerZero
+    );
+
+    SequentialGroup PPGtoPGP = new SequentialGroup();
+    SequentialGroup GPPtoPGP = new SequentialGroup();
+    SequentialGroup PGPtoPPG = new SequentialGroup();
+    SequentialGroup GPPtoPPG = new SequentialGroup();
+    SequentialGroup PPGtoGPP = new SequentialGroup();
+    SequentialGroup PGPtoGPP = new SequentialGroup();
+
+
+    SwitchCommand autoAirsortRed = new SwitchCommand(() -> "")
+            .withCase("PGP1", PPGtoPGP)
+            .withCase("PGP3", GPPtoPGP)
+            .withCase("PPG2", PGPtoPPG)
+            .withCase("PPG3", GPPtoPPG)
+            .withCase("GPP1", PPGtoGPP)
+            .withCase("GPP2", PGPtoGPP)
+            .withDefault(new SetPower(transfer, 1));
+
+
+
+
+    @Override
+    public void initialize() {
+
+        hoodServo1n= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
+        hoodServo2n=  ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
+        transfer = new MotorEx("transfer").reversed();
+
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}

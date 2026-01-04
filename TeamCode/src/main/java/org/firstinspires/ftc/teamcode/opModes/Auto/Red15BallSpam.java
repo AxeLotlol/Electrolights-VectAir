809770f3e696efb0f6b6b14c043e3744d176ddfa
AxeLotlol <mokshnot5@gmail.com>
2026-01-04 12:11:13 -0600

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/Red15BallSpam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/Red15BallSpam.java
@@ -144,11 +144,7 @@ public class Red15BallSpam extends NextFTCOpMode {
                 transfer2.setPosition(0.7);
             });
 
-<<<<<<< Updated upstream
-    public SequentialGroup shoot = new SequentialGroup(opentransfer, new Delay(0.35), transferOn, new Delay(0.8), transferOff, closeTransfer);
-=======
     public SequentialGroup shoot = new SequentialGroup(opentransfer, new Delay(0.35), transferOn, new Delay(1), transferOff, closeTransfer);
->>>>>>> Stashed changes
 
     public boolean spinup = true;
     public Command spinupfalse = new LambdaCommand()

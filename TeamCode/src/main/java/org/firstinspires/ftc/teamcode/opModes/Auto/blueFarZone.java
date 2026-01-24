65bee0bcb3934508d07423915a5a345f501ec12a
sonumanu173-debug <sonumanu173@gmail.com>
2026-01-23 23:25:41 -0600

    autooooo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/blueFarZone.java
@@ -0,0 +1,326 @@
+package org.firstinspires.ftc.teamcode.opModes.Auto;
+
+
+import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
+import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.DistanceRed;
+import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.impl.Direction;
+import dev.nextftc.hardware.impl.IMUEx;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.impl.ServoEx;
+
+
+@Autonomous(name = "Red Auto 15 Ball SPam Linear", group = "Autonomous")
+@Configurable
+public class blueFarZone extends NextFTCOpMode {
+    public blueFarZone() {
+        addComponents(
+                new SubsystemComponent(Flywheel.INSTANCE, DistanceRed.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE,
+                new PedroComponent(hwMap -> Constants.createFollower(hwMap))
+
+
+        );
+    }
+
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
+
+
+    private Paths paths;
+    public MotorEx intakeMotor;
+
+    int tagId = 0;
+
+    public Pose start = new Pose(119.4, 126.4, Math.toRadians(49));
+
+
+    private MotorEx transfer1;
+
+    private ServoEx transfer2;
+
+    public static MotorEx flywheel = new MotorEx("launchingmotor");
+
+    public static MotorEx flywheel2 = new MotorEx("launchingmotor2");
+
+   /* private CRServo hoodServo1n;
+    private CRServo hoodServo2n;
+
+    private CRServoEx hoodServo1 = new CRServoEx(() -> hoodServo1n);
+    private CRServoEx hoodServo2 = new CRServoEx(() -> hoodServo2n);
+
+
+
+
+
+
+    ParallelGroup HoodRunUp=new ParallelGroup(
+            new SetPower(hoodServo1,-1),
+            new SetPower(hoodServo2,1)
+    );
+
+    public ParallelGroup HoodPowerZero=new ParallelGroup(
+            new SetPower(hoodServo1,0),
+            new SetPower(hoodServo2,0)
+    );
+
+    public SequentialGroup HoodUp=new SequentialGroup(
+            HoodRunUp,
+            new Delay(0.18),
+            HoodPowerZero
+    );
+
+    ParallelGroup HoodRunDown=new ParallelGroup(
+            new SetPower(hoodServo1,1),
+            new SetPower(hoodServo2,-1)
+    );
+
+    public SequentialGroup HoodDown=new SequentialGroup(
+            HoodRunDown,
+            new Delay(0.17),
+            HoodPowerZero
+    );
+
+    public SequentialGroup HoodUpAuto=new SequentialGroup(
+            HoodRunUp,
+            new Delay(0.12),
+            HoodPowerZero
+    );*/
+
+    private Boolean preloadspin;
+
+    private double preloadtps;
+
+    private double shoottps;
+    Command findTPSShoot = new LambdaCommand()
+            .setStart(() -> shoottps = findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+    Command findTPSPreload = new LambdaCommand()
+            .setStart(() -> preloadtps = findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+
+    public void onInit() {
+        telemetry.addLine("Initializing Follower...");
+
+        telemetry.update();
+        follower = PedroComponent.follower();
+        /*Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(APRILTAG_PIPELINE);
+        limelight.start();*/
+
+
+        IMUEx imu = new IMUEx("imu", Direction.LEFT, Direction.BACKWARD).zeroed();
+
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(7);
+        limelight.start();
+        paths = new Paths(follower);
+        intakeMotor = new MotorEx("intake");
+        transfer1 = new MotorEx("transfer");
+        transfer2 = new ServoEx("transferServo1");
+        //hoodServo1n= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
+        // hoodServo2n=  ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
+        pathTimer = new Timer();
+        actionTimer = new Timer();
+        opmodeTimer = new Timer();
+        follower.setStartingPose(start);
+
+
+        pathState = 0;
+        telemetry.addLine("Follower + IMU + Odo Pods initialized successfully!");
+        telemetry.addLine("Initialization complete!");
+        telemetry.update();
+
+        follower.update();
+
+
+    }
+
+    private Command intakeMotorOn = new LambdaCommand()
+            .setStart(() -> intakeMotor.setPower(-1));
+
+    Command intakeMotorOff = new LambdaCommand()
+            .setStart(() -> intakeMotor.setPower(0));
+
+    double distance;
+
+    Command transferOn = new LambdaCommand()
+            .setStart(() -> transfer1.setPower(-1));
+    Command transferOff = new LambdaCommand()
+            .setStart(() -> transfer1.setPower(0));
+    Command transferOnForIntake = new LambdaCommand()
+            .setStart(() -> transfer1.setPower(-1));
+
+    public Command opentransfer = new LambdaCommand()
+            .setStart(() -> {
+                //`5transfer2.setPosition(-0.25);
+                transfer2.setPosition(0.3);
+            });
+
+    public Command closeTransfer = new LambdaCommand()
+            .setStart(() -> {
+                transfer2.setPosition(0.7);
+            });
+
+    public SequentialGroup shoot = new SequentialGroup(opentransfer, new Delay(0.35), transferOn, new Delay(0.87), transferOff, closeTransfer);
+
+    public boolean spinup = true;
+    public Command spinupfalse = new LambdaCommand()
+            .setStart(() -> {
+                spinup = false;
+            });
+
+    Command spinupPLEASEEIsagiINEEDTHIS = new LambdaCommand()
+            .setStart(() -> {
+                flywheel.setPower(1);
+                flywheel2.setPower(-1);
+
+            });
+    public Command reverseIntakeForMe = new LambdaCommand()
+            .setStart(() -> intakeMotor.setPower(0.5));
+
+    public boolean lift;
+    boolean lowerangle = false;
+    private Boolean preloadspinreal = false;
+
+    Command preloadSpun = new LambdaCommand().setStart(() -> preloadspinreal = true);
+    Command preloadSpunReal = new LambdaCommand().setStart(() -> preloadspinreal = false);
+
+
+    public Command Auto() {
+        return new SequentialGroup(
+                new FollowPath(paths.Shoot1),
+                shoot,
+                new Delay(0.5),
+                intakeMotorOn,
+                transferOnForIntake,
+                new FollowPath(paths.Intake1),
+                new Delay(0.5),
+                intakeMotorOff,
+                new FollowPath(paths.Shoot2),
+                shoot,
+                new Delay(0.5),
+                new FollowPath(paths.Leave)
+
+
+        );
+    }
+
+    public static boolean farzoneBlue = false;
+
+    public void onStartButtonPressed() {
+        opmodeTimer.resetTimer();
+        pathTimer.resetTimer();
+        /*flywheel.setPower(1);
+        flywheel2.setPower(-1);*/
+
+        preloadspinreal = true;
+        shooter(2095);
+
+        //int tag=MotifScanning.INSTANCE.findMotif();
+        Auto().schedule();
+
+        farzoneBlue = true;
+
+
+    }
+
+    @Override
+    public void onUpdate() {
+        follower.update();
+
+        if (preloadspinreal) {
+            shooter(2095);
+        } else {
+            if (DistanceRed.INSTANCE.getDistanceFromTag() != 0) {
+                shooter(findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+                ActiveOpMode.telemetry().addData("Limelight!", findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+            } else if (DistanceRed.INSTANCE.getDistanceFromTag() == 0) {
+                shooter(2095);
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onStop() {
+        follower.breakFollowing();
+        telemetry.addLine("Autonomous Stopped.");
+        telemetry.update();
+    }
+
+    public class Paths {
+        public PathChain Shoot1;
+        public PathChain Intake1;
+        public PathChain Shoot2;
+        public PathChain Leave;
+
+        public Paths(Follower follower) {
+            Shoot1 = follower.pathBuilder().addPath(
+                            new BezierLine(
+                                    new Pose(80.837, 8.692),
+
+                                    new Pose(84.104, 16.008)
+                            )
+                    ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(65))
+
+                    .build();
+
+            Intake1 = follower.pathBuilder().addPath(
+                            new BezierCurve(
+                                    new Pose(84.104, 16.008),
+                                    new Pose(113.052, 9.595),
+                                    new Pose(135.102, 9.293)
+                            )
+                    ).setTangentHeadingInterpolation()
+
+                    .build();
+
+            Shoot2 = follower.pathBuilder().addPath(
+                            new BezierCurve(
+                                    new Pose(135.102, 9.293),
+                                    new Pose(82.321, 12.129),
+                                    new Pose(84.104, 16.008)
+                            )
+                    ).setLinearHeadingInterpolation(Math.toRadians(355), Math.toRadians(65))
+
+                    .build();
+
+            Leave = follower.pathBuilder().addPath(
+                            new BezierLine(
+                                    new Pose(84.104, 16.008),
+
+                                    new Pose(105.245, 11.956)
+                            )
+                    ).setLinearHeadingInterpolation(Math.toRadians(65), Math.toRadians(90))
+
+                    .build();
+        }
+    }
+}

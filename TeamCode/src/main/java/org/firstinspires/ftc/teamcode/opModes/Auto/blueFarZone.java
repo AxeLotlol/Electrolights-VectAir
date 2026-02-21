39e1a6db4f353fdca3f79657bbe09b04179d8c42
AxeLotlol <mokshnot5@gmail.com>
2026-02-21 07:49:23 -0600

    Farzoner is dpone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/blueFarZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/blueFarZone.java
@@ -1,31 +1,37 @@
 package org.firstinspires.ftc.teamcode.opModes.Auto;
 
-
 import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
 import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;
+import static org.firstinspires.ftc.teamcode.subsystems.AutoShooterCalc.calculateShotVectorandUpdateHeading;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.math.Vector;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-import org.firstinspires.ftc.teamcode.subsystems.DistanceRed;
+import org.firstinspires.ftc.teamcode.subsystems.DistanceBlue;
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.FollowPath;
 import dev.nextftc.extensions.pedro.PedroComponent;
+
+import dev.nextftc.extensions.pedro.TurnTo;
+
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
@@ -33,19 +39,17 @@ import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.IMUEx;
 import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.impl.ServoEx;
-
+import dev.nextftc.hardware.positionable.SetPosition;
 
 @Autonomous
 @Configurable
 public class blueFarZone extends NextFTCOpMode {
     public blueFarZone() {
         addComponents(
-                new SubsystemComponent(Flywheel.INSTANCE, DistanceRed.INSTANCE),
+                new SubsystemComponent(Flywheel.INSTANCE, DistanceBlue.INSTANCE),
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE,
                 new PedroComponent(hwMap -> Constants.createFollower(hwMap))
-
-
         );
     }
 
@@ -53,14 +57,12 @@ public class blueFarZone extends NextFTCOpMode {
     private Timer pathTimer, actionTimer, opmodeTimer;
     private int pathState;
 
-
     private Paths paths;
     public MotorEx intakeMotor;
 
     int tagId = 0;
 
-    public Pose start = new Pose(80.837, 8.692, Math.toRadians(49));
-
+    public Pose start = new Pose(63.163, 8.692, Math.toRadians(90));
 
     private MotorEx transfer1;
 
@@ -70,69 +72,29 @@ public class blueFarZone extends NextFTCOpMode {
 
     public static MotorEx flywheel2 = new MotorEx("launchingmotor2");
 
-   /* private CRServo hoodServo1n;
-    private CRServo hoodServo2n;
-
-    private CRServoEx hoodServo1 = new CRServoEx(() -> hoodServo1n);
-    private CRServoEx hoodServo2 = new CRServoEx(() -> hoodServo2n);
-
-
-
-
-
-
-    ParallelGroup HoodRunUp=new ParallelGroup(
-            new SetPower(hoodServo1,-1),
-            new SetPower(hoodServo2,1)
-    );
-
-    public ParallelGroup HoodPowerZero=new ParallelGroup(
-            new SetPower(hoodServo1,0),
-            new SetPower(hoodServo2,0)
-    );
-
-    public SequentialGroup HoodUp=new SequentialGroup(
-            HoodRunUp,
-            new Delay(0.18),
-            HoodPowerZero
-    );
-
-    ParallelGroup HoodRunDown=new ParallelGroup(
-            new SetPower(hoodServo1,1),
-            new SetPower(hoodServo2,-1)
-    );
-
-    public SequentialGroup HoodDown=new SequentialGroup(
-            HoodRunDown,
-            new Delay(0.17),
-            HoodPowerZero
-    );
-
-    public SequentialGroup HoodUpAuto=new SequentialGroup(
-            HoodRunUp,
-            new Delay(0.12),
-            HoodPowerZero
-    );*/
-
     private Boolean preloadspin;
 
     private double preloadtps;
 
     private double shoottps;
     Command findTPSShoot = new LambdaCommand()
-            .setStart(() -> shoottps = findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+            .setStart(() -> shoottps = findTPS(DistanceBlue.INSTANCE.getDistanceFromTag()));
     Command findTPSPreload = new LambdaCommand()
-            .setStart(() -> preloadtps = findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
+            .setStart(() -> preloadtps = findTPS(DistanceBlue.INSTANCE.getDistanceFromTag()));
+    private static Servo hoodServo1n;
+    private static Servo hoodServo2n;
+
+    private static ServoEx hoodServo1 = new ServoEx(() -> hoodServo1n);
+    private static ServoEx hoodServo2 = new ServoEx(() -> hoodServo2n);
 
     public void onInit() {
         telemetry.addLine("Initializing Follower...");
 
         telemetry.update();
         follower = PedroComponent.follower();
-        /*Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(APRILTAG_PIPELINE);
-        limelight.start();*/
 
+        hoodServo1n= ActiveOpMode.hardwareMap().get(Servo.class, "hoodServo1");
+        hoodServo2n=  ActiveOpMode.hardwareMap().get(Servo.class, "hoodServo2");
 
         IMUEx imu = new IMUEx("imu", Direction.LEFT, Direction.BACKWARD).zeroed();
 
@@ -143,13 +105,11 @@ public class blueFarZone extends NextFTCOpMode {
         intakeMotor = new MotorEx("intake");
         transfer1 = new MotorEx("transfer");
         transfer2 = new ServoEx("transferServo1");
-        //hoodServo1n= ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo1");
-        // hoodServo2n=  ActiveOpMode.hardwareMap().get(CRServo.class, "hoodServo2");
+
         pathTimer = new Timer();
         actionTimer = new Timer();
         opmodeTimer = new Timer();
         follower.setStartingPose(start);
-
 
         pathState = 0;
         telemetry.addLine("Follower + IMU + Odo Pods initialized successfully!");
@@ -157,8 +117,6 @@ public class blueFarZone extends NextFTCOpMode {
         telemetry.update();
 
         follower.update();
-
-
     }
 
     private Command intakeMotorOn = new LambdaCommand()
@@ -171,23 +129,24 @@ public class blueFarZone extends NextFTCOpMode {
 
     Command transferOn = new LambdaCommand()
             .setStart(() -> transfer1.setPower(-1));
+
     Command transferOff = new LambdaCommand()
             .setStart(() -> transfer1.setPower(0));
+
     Command transferOnForIntake = new LambdaCommand()
             .setStart(() -> transfer1.setPower(-1));
 
     public Command opentransfer = new LambdaCommand()
             .setStart(() -> {
-                //`5transfer2.setPosition(-0.25);
                 transfer2.setPosition(0.3);
             });
 
     public Command closeTransfer = new LambdaCommand()
             .setStart(() -> {
-                transfer2.setPosition(0.7);
+                transfer2.setPosition(0.635);
             });
 
-    public SequentialGroup shoot = new SequentialGroup(opentransfer, new Delay(0.35), transferOn, new Delay(0.87), transferOff, closeTransfer);
+    public SequentialGroup shoot = new SequentialGroup(opentransfer, new Delay(0.05), transferOn, new Delay(0.4), transferOff, closeTransfer);
 
     public boolean spinup = true;
     public Command spinupfalse = new LambdaCommand()
@@ -199,8 +158,8 @@ public class blueFarZone extends NextFTCOpMode {
             .setStart(() -> {
                 flywheel.setPower(1);
                 flywheel2.setPower(-1);
-
             });
+
     public Command reverseIntakeForMe = new LambdaCommand()
             .setStart(() -> intakeMotor.setPower(0.5));
 
@@ -208,26 +167,61 @@ public class blueFarZone extends NextFTCOpMode {
     boolean lowerangle = false;
     private Boolean preloadspinreal = false;
 
+    public Command turnTo(Pose targetPose, double timeoutSeconds) {
+        return new LambdaCommand("Turn to " + targetPose.getHeading())
+                .setStart(() -> follower.holdPoint(targetPose))
+                .setIsDone(() -> false)
+                .raceWith(new Delay(timeoutSeconds));
+    }
+
     Command preloadSpun = new LambdaCommand().setStart(() -> preloadspinreal = true);
     Command preloadSpunReal = new LambdaCommand().setStart(() -> preloadspinreal = false);
 
+    public static double hoodToPos(double runtime) {
+        if(!Double.isNaN(runtime)) {
+            ActiveOpMode.telemetry().addData("runtime", runtime);
+            ParallelGroup HoodRunUp = new ParallelGroup(
+                    new SetPosition(hoodServo1, runtime),
+                    new SetPosition(hoodServo2, -1*runtime)
+            );
+            HoodRunUp.schedule();
+            return runtime;
+        }
+        else {
+            ActiveOpMode.telemetry().addLine("NaN");
+            return 0;
+        }
+    }
 
     public Command Auto() {
         return new SequentialGroup(
-                new FollowPath(paths.Shoot1),
+                new FollowPath(paths.Path1),
+                new Delay(2),
                 shoot,
                 new Delay(0.5),
                 intakeMotorOn,
                 transferOnForIntake,
-                new FollowPath(paths.Intake1),
-                new Delay(0.5),
+
+                new FollowPath(paths.Path2),
+                new Delay(2.0),
                 intakeMotorOff,
-                new FollowPath(paths.Shoot2),
+                new FollowPath(paths.Path3),
                 shoot,
                 new Delay(0.5),
-                new FollowPath(paths.Leave)
-
-
+                intakeMotorOn,
+                new FollowPath(paths.Path2),
+                new Delay(2.0),
+                intakeMotorOff,
+                new FollowPath(paths.Path3),
+                shoot,
+                new Delay(0.5),
+                intakeMotorOn,
+                new FollowPath(paths.Path2),
+                new Delay(2.0),
+                intakeMotorOff,
+                new FollowPath(paths.Path3),
+                shoot,
+                new FollowPath(paths.Path4,true)
         );
     }
 
@@ -236,37 +230,30 @@ public class blueFarZone extends NextFTCOpMode {
     public void onStartButtonPressed() {
         opmodeTimer.resetTimer();
         pathTimer.resetTimer();
-        /*flywheel.setPower(1);
-        flywheel2.setPower(-1);*/
 
         preloadspinreal = true;
-        shooter(2095);
+        shooter(1500);
 
-        //int tag=MotifScanning.INSTANCE.findMotif();
         Auto().schedule();
-
         farzoneBlue = true;
-
-
     }
 
     @Override
     public void onUpdate() {
         follower.update();
 
-        if (preloadspinreal) {
-            shooter(2095);
-        } else {
-            if (DistanceRed.INSTANCE.getDistanceFromTag() != 0) {
-                shooter(findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
-                ActiveOpMode.telemetry().addData("Limelight!", findTPS(DistanceRed.INSTANCE.getDistanceFromTag()));
-            } else if (DistanceRed.INSTANCE.getDistanceFromTag() == 0) {
-                shooter(2095);
-            }
-        }
+        Pose currPose = follower.getPose();
+        double robotHeading = follower.getPose().getHeading();
 
+        // Transformed the goal from (6, 138) to (144-6, 138) -> (138, 138)
+        Vector robotToGoalVector = new Vector(follower.getPose().distanceFrom(new Pose(138, 138)), Math.atan2(138 - currPose.getY(), 138 - currPose.getX()));
+
+        Double[] results = calculateShotVectorandUpdateHeading(robotHeading, robotToGoalVector, follower.getVelocity());
+        double flywheelSpeed = results[0];
+        shooter((float) flywheelSpeed);
+        double hoodAngle = results[1];
+        hoodToPos(hoodAngle);
     }
-
 
     @Override
     public void onStop() {
@@ -276,50 +263,45 @@ public class blueFarZone extends NextFTCOpMode {
     }
 
     public class Paths {
-        public PathChain Shoot1;
-        public PathChain Intake1;
-        public PathChain Shoot2;
-        public PathChain Leave;
+        public PathChain Path1;
+        public PathChain Path2;
+        public PathChain Path3;
+        public PathChain Path4;
 
         public Paths(Follower follower) {
-            Shoot1 = follower.pathBuilder().addPath(
+            Path1 = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(80.837, 8.692),
-
-                                    new Pose(84.104, 16.008)
+                                    new Pose(56.5, 8.1),
+                                    new Pose(58, 13)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(65))
-
+                    ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(112))
                     .build();
 
-            Intake1 = follower.pathBuilder().addPath(
+            Path2 = follower.pathBuilder().addPath(
                             new BezierCurve(
-                                    new Pose(84.104, 16.008),
-                                    new Pose(113.052, 9.595),
-                                    new Pose(135.102, 9.293)
+                                    new Pose(58, 13),
+                                    new Pose(47.4775, 34.473),
+                                    new Pose(46.44, 10.498),
+                                    new Pose(21, 8)
                             )
-                    ).setTangentHeadingInterpolation()
-
+                    ).setLinearHeadingInterpolation(Math.toRadians(112), Math.toRadians(183))
                     .build();
 
-            Shoot2 = follower.pathBuilder().addPath(
-                            new BezierCurve(
-                                    new Pose(135.102, 9.293),
-                                    new Pose(82.321, 12.129),
-                                    new Pose(84.104, 16.008)
-                            )
-                    ).setLinearHeadingInterpolation(Math.toRadians(355), Math.toRadians(65))
-
-                    .build();
-
-            Leave = follower.pathBuilder().addPath(
+            Path3 = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(84.104, 16.008),
-
-                                    new Pose(105.245, 11.956)
+                                    new Pose(21, 8),
+                                    new Pose(58, 13)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(65), Math.toRadians(90))
+                    ).setLinearHeadingInterpolation(Math.toRadians(183), Math.toRadians(110))
+                    .build();
 
+            Path4 = follower.pathBuilder().addPath(
+                            new BezierLine(
+                                    new Pose(58, 13),
+                                    new Pose(38, 10)
+                            )
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(113), Math.toRadians(88))
                     .build();
         }
     }

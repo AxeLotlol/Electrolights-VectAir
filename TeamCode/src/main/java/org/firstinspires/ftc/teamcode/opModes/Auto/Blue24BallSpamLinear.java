b513f00a908f25c4cdddaf1773efbd0cbdaa75ba
Mithun Dhanes <mithun1214.d01@gmail.com>
2026-06-08 17:12:52 -0500

    New Auto Paths. need to fix ramming into the classifier during spike 2 intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/Blue24BallSpamLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Auto/Blue24BallSpamLinear.java
@@ -21,12 +21,11 @@ import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 
-
-@Autonomous 
+@Autonomous
 @Configurable
-public class Red24BallSpamLinear extends NextFTCOpMode {
+public class Blue24BallSpamLinear extends NextFTCOpMode {
 
-    public Red24BallSpamLinear() {
+    public Blue24BallSpamLinear() {
         addComponents(
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE,
@@ -39,8 +38,7 @@ public class Red24BallSpamLinear extends NextFTCOpMode {
     private Paths paths;
     public static double gateTime = 1.32;
 
-
-    public Pose start = new Pose(110.58652658884565, 133.1659559014267, Math.toRadians(270));
+    public Pose start = new Pose(33.41347341115435, 133.1659559014267, Math.toRadians(270));
 
     public void onInit() {
         follower = PedroComponent.follower();
@@ -115,13 +113,11 @@ public class Red24BallSpamLinear extends NextFTCOpMode {
         public PathChain park;
 
         public Paths(Follower follower) {
-
-
             preLoadShoot = follower.pathBuilder()
                     .addPath(
                             new BezierLine(
-                                    new Pose(110.587, 133.166),
-                                    new Pose(97.723, 87.429)
+                                    new Pose(33.413, 133.166),
+                                    new Pose(46.277, 87.429)
                             )
                     )
                     .setTValueConstraint(0.8)
@@ -131,96 +127,92 @@ public class Red24BallSpamLinear extends NextFTCOpMode {
             spike2 = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(97.723, 87.429),
-                                    new Pose(85.512, 58.943),
-                                    new Pose(104.243, 59.193)
+                                    new Pose(46.277, 87.429),
+                                    new Pose(58.488, 58.943),
+                                    new Pose(39.757, 59.193)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(0))
+                    .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(180))
                     .addPath(
                             new BezierLine(
-                                    new Pose(104.243, 59.193),
-                                    new Pose(126.044, 59.193)
+                                    new Pose(39.757, 59.193),
+                                    new Pose(17.956, 59.193)
                             )
                     )
                     .setTValueConstraint(0.8)
                     .setTangentHeadingInterpolation()
                     .build();
-
 
             launchSpike2 = follower.pathBuilder()
                     .addPath(
                             new BezierLine(
-                                    new Pose(126.044, 59.193),
-                                    new Pose(84.228, 69.272)
+                                    new Pose(17.956, 59.193),
+                                    new Pose(59.772, 69.272)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .setConstantHeadingInterpolation(Math.toRadians(180))
                     .build();
-
 
             gate1 = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(84.228, 69.272),
-                                    new Pose(101.121, 49.603),
-                                    new Pose(134.754, 60.596)
+                                    new Pose(59.772, 69.272),
+                                    new Pose(42.879, 49.603),
+                                    new Pose(9.246, 60.596)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(20))
+                    .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(160))
                     .build();
-
 
             openForPartner = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(134.754, 60.596),
-                                    new Pose(131.710, 59.133),
-                                    new Pose(131.154, 59.202),
-                                    new Pose(130.639, 59.300),
-                                    new Pose(130.164, 59.427),
-                                    new Pose(129.729, 59.584),
-                                    new Pose(129.335, 59.770),
-                                    new Pose(128.981, 59.986),
-                                    new Pose(128.667, 60.230),
-                                    new Pose(128.394, 60.505),
-                                    new Pose(128.161, 60.808),
-                                    new Pose(127.968, 61.141),
-                                    new Pose(127.816, 61.503),
-                                    new Pose(127.704, 61.895),
-                                    new Pose(131.138, 65.082)
+                                    new Pose(9.246, 60.596),
+                                    new Pose(12.290, 59.133),
+                                    new Pose(12.846, 59.202),
+                                    new Pose(13.361, 59.300),
+                                    new Pose(13.836, 59.427),
+                                    new Pose(14.271, 59.584),
+                                    new Pose(14.665, 59.770),
+                                    new Pose(15.019, 59.986),
+                                    new Pose(15.333, 60.230),
+                                    new Pose(15.606, 60.505),
+                                    new Pose(15.839, 60.808),
+                                    new Pose(16.032, 61.141),
+                                    new Pose(16.184, 61.503),
+                                    new Pose(16.296, 61.895),
+                                    new Pose(12.862, 65.082)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .setConstantHeadingInterpolation(Math.toRadians(180))
                     .build();
-
 
             spike1 = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(131.138, 65.082),
-                                    new Pose(95.715, 67.249),
-                                    new Pose(96.535, 82.990)
+                                    new Pose(12.862, 65.082),
+                                    new Pose(48.285, 67.249),
+                                    new Pose(47.465, 82.990)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .setConstantHeadingInterpolation(Math.toRadians(180))
                     .addPath(
                             new BezierLine(
-                                    new Pose(96.535, 82.990),
-                                    new Pose(123.903, 82.439)
+                                    new Pose(47.465, 82.990),
+                                    new Pose(20.097, 82.439)
                             )
                     )
                     .setTValueConstraint(0.8)
                     .setTangentHeadingInterpolation()
                     .addPath(
                             new BezierLine(
-                                    new Pose(123.903, 82.439),
-                                    new Pose(96.535, 82.439)
+                                    new Pose(20.097, 82.439),
+                                    new Pose(47.465, 82.439)
                             )
                     )
                     .setTValueConstraint(0.8)
@@ -228,55 +220,51 @@ public class Red24BallSpamLinear extends NextFTCOpMode {
                     .setReversed()
                     .build();
 
-
             gate2Intake = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(96.535, 82.439),
-                                    new Pose(101.121, 49.603),
-                                    new Pose(134.754, 60.596)
+                                    new Pose(47.465, 82.439),
+                                    new Pose(42.879, 49.603),
+                                    new Pose(9.246, 60.596)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(20))
+                    .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(160))
                     .build();
-
 
             gateIntake = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(83.921, 69.318),
-                                    new Pose(101.121, 49.603),
-                                    new Pose(134.754, 60.596)
+                                    new Pose(60.079, 69.318),
+                                    new Pose(42.879, 49.603),
+                                    new Pose(9.246, 60.596)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(20))
+                    .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(160))
                     .build();
-
 
             gateShoot = follower.pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(131.138, 65.082),
-                                    new Pose(95.715, 67.249),
-                                    new Pose(83.921, 69.318)
+                                    new Pose(12.862, 65.082),
+                                    new Pose(48.285, 67.249),
+                                    new Pose(60.079, 69.318)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setLinearHeadingInterpolation(Math.toRadians(20), Math.toRadians(0))
+                    .setLinearHeadingInterpolation(Math.toRadians(160),Math.toRadians(180))
                     .build();
-
 
             park = follower.pathBuilder()
                     .addPath(
                             new BezierLine(
-                                    new Pose(83.921, 69.318),
-                                    new Pose(128.904, 83.589)
+                                    new Pose(60.079, 69.318),
+                                    new Pose(15.096, 83.589)
                             )
                     )
                     .setTValueConstraint(0.8)
-                    .setConstantHeadingInterpolation(Math.toRadians(0))
+                    .setConstantHeadingInterpolation(Math.toRadians(180))
                     .build();
         }
     }

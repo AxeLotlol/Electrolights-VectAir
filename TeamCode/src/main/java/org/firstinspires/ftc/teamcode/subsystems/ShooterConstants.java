76e21c17a3302d1f6060aacf8c76eebc859aba4d
AxeLotlol <mokshnot5@gmail.com>
2026-07-01 21:37:46 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterConstants.java
@@ -7,7 +7,7 @@ import com.pedropathing.geometry.Pose;
 public class ShooterConstants {
     public static Pose GOAL_POS_RED = new Pose(144, 144);
     public static Pose GOAL_POS_BLUE = GOAL_POS_RED.mirror();
-    public static double SCORE_HEIGHT = 33; //inches
+    public static double SCORE_HEIGHT = 32.75; //inches
     public static double SCORE_HEIGHT_CLOSER = 34; //inches
 
     public static double SCORE_ANGLE = -17;
